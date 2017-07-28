c28d54ee60abfd12d7edf779e424bf35d72d8170
Ytalo Elias Borja Mori <ucabyeb@ucl.ac.uk>
2017-07-28 23:01:37 +0100

    Fixing the problem of downloading the csv file on all browsers.

--- anonymiser-web/src/download.js
+++ anonymiser-web/src/download.js
@@ -2,10 +2,13 @@ const csvContentType = 'text/csv';
 
 export const downloadCSVFile = (filename, content) => {
   let a = document.createElement('a');
+
   let blob = new Blob([content], {
     'type': csvContentType,
   });
-  a.href = window.URL.createObjectURL(blob);
-  a.download = filename;
+  a.setAttribute('href', window.URL.createObjectURL(blob));
+  a.setAttribute('download', filename);
+  document.body.appendChild(a);
   a.click();
+  document.body.removeChild(a);
 };
